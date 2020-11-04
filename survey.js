de691ce14f31d148ee7b77ae2799302287ddcf4b
Pavel Piatetskii <pavel.piatetskii@gmail.com>
2020-11-04 19:15:14 +0000

    Refactored profile-generator. Now it is a generalized function which can take any object containing a survey and desired output and do not depends on outer counters

--- survey.js
+++ survey.js
@@ -5,40 +5,40 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const survey = ['What\'s your name? ', 
-                'What\'s an activity you like doing? ',
-                'What do you listen to while doing that? ',
-                'Which meal is your favourite (eg: dinner, brunch, etc.) ',
-                'What\'s your favourite thing to eat for that meal? ',
-                'Which sport is your absolute favourite? ',
-                'What is your superpower? In a few words, tell us what you are amazing at! '];
-const answers = [];
-let start = 0;
+let profileMaker = {
+  questions:  [
+               'What\'s your name? ', 
+               'What\'s an activity you like doing? ',
+               'What do you listen to while doing that? ',
+               'Which meal is your favourite (eg: dinner, brunch, etc.) ',
+               'What\'s your favourite thing to eat for that meal? ',
+               'Which sport is your absolute favourite? ',
+               'What is your superpower? In a few words, tell us what you are amazing at! '
+              ],
+    answers:  [
+               'name', 'activity', 'music', 'meal', 'food', 'sport', 'superpower'
+              ],
+printResult:  function() {console.log(`${this.name} loves listening to ${this.music} ` +
+                `while ${this.activity}, devouring ${this.food} for ${this.meal}, ` +
+                `prefers ${this.sport} over any other sport, and is ` +
+                `amazing at ${this.superpower}.`)},
+          i:  0
+};
 
-const profile = function() {
-  console.log(`${answers[0]} loves listening to ${answers[2]} while ${answers[1]},`+ 
-`devouring ${answers[4]} for ${answers[3]}, prefers ${answers[5]} over any other `+
-`sport, and is amazing at ${answers[6]}`)
+const surveyHandler = function(data) {
+
+  rl.question(data.questions[data.i], (answer) => { 
+    data[ data.answers[data.i] ] = answer;
+
+    if (data.i === data.questions.length - 1) { 
+      rl.close()
+      data.printResult();
+
+    } else {
+      data.i++;
+      surveyHandler(data);
+    }
+  })
 }
 
-const fun = function(question, i) {
-
-  if (i === question.length - 1) {
-    return rl.question(question[i], (answer) => {
-      answers.push(answer);
-      console.log(`Question number ${i} ${answer}`);
-      rl.close();
-      profile();
-    })
-
-  } else {
-    rl.question(question[i], (answer) => {
-      answers.push(answer);
-      console.log(`Question number ${i} ${answer}`)
-      i++;
-      fun(question, i);
-    })
-  }
-}
-
-fun(survey, start);
+surveyHandler(profileMaker);
